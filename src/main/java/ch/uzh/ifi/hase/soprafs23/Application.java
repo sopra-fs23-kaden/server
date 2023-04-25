e2670332e47a4107ffbe2fec2668fad9d44f9e9d
hk-kaden-kim <hyeongkyun.kim@uzh.ch>
2023-04-25 17:33:03 +0200

    Secret Key Issue

--- src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/Application.java
@@ -21,7 +21,20 @@ public class Application {
   @ResponseStatus(HttpStatus.OK)
   @ResponseBody
   public String helloWorld() {
-    return "The application is running.";
+      /*
+            [KADEN] ADDED/EDITED FOR Github Secret Key
+
+            For running on local machine, set an env global variable INVISIBLE_KEY = <<<Same as github secret value>>>
+            when running gradle daemon
+            e.g. ./gradlew bootRun
+                 >>> INVISIBLE_KEY=<<localmachine-secret-setting>> ./gradlew bootRun
+
+            For running on GCP, an env global variable will be automatically set by using main.yml on .github/workflows
+      */
+      String secret="initialize";
+      secret = System.getenv("INVISIBLE_KEY");
+      System.out.println("LOCAL SECRET KEY CHECKPOINT " + secret);
+      return "The application is running. " + secret;
   }
 
   @Bean
